4a18004f4bdd747d107a065a6762888625d1a37f
Mark <Mark@MAHSHEV>
2014-05-16 19:26:33 +0300

    communication

--- src/communication/CommunicationManager.java
+++ src/communication/CommunicationManager.java
@@ -1,8 +1,17 @@
 package communication;
 
+import gnu.io.CommPort;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
 public class CommunicationManager {
 
 	private static CommunicationManager instance = null;
+	private SerialPort serialPort;
 	
 	private CommunicationManager() {
 		
@@ -12,5 +21,33 @@ public class CommunicationManager {
 		if (instance == null) 
 			instance = new CommunicationManager();
 		return instance;
+	}
+	
+	public void connect (String portName) throws Exception {
+        CommPortIdentifier portIdentifier = CommPortIdentifier.getPortIdentifier(portName);
+        if ( portIdentifier.isCurrentlyOwned() ) {
+            System.out.println("Error: Port is currently in use");
+        }
+        else {
+            CommPort commPort = portIdentifier.open(this.getClass().getName(),2000);
+            
+            if ( commPort instanceof SerialPort ){
+                serialPort = (SerialPort) commPort;
+                serialPort.setSerialPortParams(57600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
+                
+                InputStream in = serialPort.getInputStream();
+                OutputStream out = serialPort.getOutputStream();
+                
+                (new Thread(new SerialReader(in))).start();
+                (new Thread(new SerialWriter(out))).start();
+            }
+            else {
+                System.out.println("Error: Only serial ports are handled by this application");
+            }
+        }     
+    }
+	
+	public void sendData(byte[] dataToSend) throws Exception {
+		
 	}
 }
