b1d580ea623ed717c4fb63c2c5f1a1a5dadc6b27
Mark <Mark@MAHSHEV>
2014-06-07 14:06:43 +0300

    Communication testing mains

--- src/communication/SerialReader.java
+++ src/communication/SerialReader.java
@@ -1,11 +1,7 @@
 package communication;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
 public class SerialReader implements Runnable {
 	InputStream in;
