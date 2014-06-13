5f01d55d9b05b80561ae7cd14a9ad30e56521dae
Mark <Mark@MAHSHEV>
2014-06-13 12:44:01 +0300

    Sending commands added

--- src/data/Status.java
+++ src/data/Status.java
@@ -4,7 +4,8 @@ public enum Status {
 	ON(1),
 	MALFUNCTION(2),
 	STANDBY(3), 
-	NON_OPERATIONAL(4);
+	NON_OPERATIONAL(4),
+	UNKNOWN(5);
 
 	private final int value;
 	
@@ -14,6 +15,23 @@ public enum Status {
 
 	public int getValue() { 
 		return value; 
+	}
+	
+	public String toString() {
+		switch(this) {
+		case ON:
+			return "ON";
+		case MALFUNCTION:
+			return "MALFUNCTION";
+		case STANDBY:
+			return "STANDBY";
+		case NON_OPERATIONAL:
+			return "NON_OPERATIONAL";
+		case UNKNOWN:
+			return "UNKNOWN";
+		default:
+			return null;
+		}
 	}
 
 }
